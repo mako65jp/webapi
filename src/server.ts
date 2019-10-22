72fc7ebb3093ed8d6add08c806f533760b860596
mako65jp <mako65jp@gmail.com>
2019-10-22 22:43:06 +0900

    update pg orm

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,6 @@
-// /*---------------------------------------------------------------------------------------------
-//  *  Copyright (c) Microsoft Corporation. All rights reserved.
-//  *  Licensed under the MIT License. See License.txt in the project root for license information.
-//  *--------------------------------------------------------------------------------------------*/
-
-// 'use strict';
-
-// const express = require('express');
-
-// // Constants
-// const PORT = 3000;
-// const HOST = '0.0.0.0';
-
-// // App
-// const app = express();
-// app.get('/', (req, res) => {
-// 	res.send('Hello remote world!\n');
-// });
-
-// app.listen(PORT, HOST);
-// console.log(`Running on http://${HOST}:${PORT}`);
-
 import http from "http";
 import express from 'express';
+import "reflect-metadata";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import errorHandlers from "./middleware/errorHandlers";
